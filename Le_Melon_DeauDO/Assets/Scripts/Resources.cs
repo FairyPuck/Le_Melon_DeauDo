bfaf5a432ddd8141aee3d9b4b143c9070e8e9e62
Julian Hamboyan <57135592+FairyPuck@users.noreply.github.com>
2021-12-03 17:40:59 +0100

    UI Resources

--- Le_Melon_DeauDO/Assets/Scripts/Resources.cs
+++ Le_Melon_DeauDO/Assets/Scripts/Resources.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Resources : MonoBehaviour
 {
-    public int melonJuice = 75;
-    public int metal = 50;
+    public int melonJuice = 75, metal = 50;
+    public TextMeshProUGUI juiceValue, metalValue;
+
+    private void FixedUpdate()
+    {
+        juiceValue.text = melonJuice.ToString();
+        metalValue.text = metal.ToString();
+
+    }
 }
